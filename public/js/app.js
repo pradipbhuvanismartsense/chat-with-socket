9db3df687f2a2d80e23bddb5cc9ccc2936f916a8
Pradip Bhuvani <pradip.bhuvani@smartsensesolutions.com>
2022-04-04 16:09:58 +0530

    add frontend form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,18 @@ socket.on("message", function (data) {
   console.log("Received new message ");
   console.log(data.text);
 });
+
+/** handles submitting of new message */
+var $form = jQuery("#message-form");
+
+$form.on("submit", function (event) {
+  event.preventDefault();
+  console.log("form submitted", $form.find("input[name=message]").val());
+  var $message = $form.find("input[name=message]");
+
+  socket.emit("message", {
+    text: $message.val(),
+  });
+
+  $message.val("");
+});
