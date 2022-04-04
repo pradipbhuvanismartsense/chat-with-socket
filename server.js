18845364d0a846fa2e070edfbbd6d6a364c00807
Pradip Bhuvani <pradip.bhuvani@smartsensesolutions.com>
2022-04-04 14:47:10 +0530

    add socket.io on client and saerver

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ let PORT = process.env["PORT"] || 3000;
 let express = require("express");
 let app = express();
 let http = require("http").Server(app);
+let io = require("socket.io")(http);
 
 app.use(express.static(__dirname + "/public"));
+
+io.on("connection", function (client) {
+  console.log(`New user connected via socket.io`);
+});
 
 http.listen(PORT, function () {
   console.info(`server is running on port ${PORT}`);
